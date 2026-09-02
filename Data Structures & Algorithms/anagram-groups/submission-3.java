ebf72b1fcb902c933be754ad13ff5588343000a1
tydapchai <146206537+tydapchai@users.noreply.github.com>
2026-09-02 22:26:45 +0700

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.java
@@ -0,0 +1,45 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> results = new ArrayList<>();
+        boolean[] visited = new boolean[strs.length];
+        
+        for (int i = 0; i < strs.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            List<String> group = new ArrayList<>();
+            group.add(strs[i]);
+            visited[i] = true;
+
+            for (int j = i + 1; j < strs.length; j++) {
+                if (!visited[j] && isAnagrams(strs[i], strs[j])) {
+                    group.add(strs[j]);
+                    visited[j] = true;
+                }
+            }
+            result.add(group);
+        }
+        return result;
+    }
+
+    public boolean isAnagrams(String s, String t) {
+    if (s.length() != t.length()) {
+        return false;
+    }
+
+    int[] count = new int[26];
+    for (int i = 0; i < s.length(); i++) {
+        count[s.charAt(i) - 'a']++;
+        count[t.charAt(i) - 'a']--;
+    }
+    for (int c : count) {
+        if (c != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+}
+
+
