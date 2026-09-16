9cf896eb2f77d6e9d63d17af6a0b7cb0e201b9d8
tydapchai <146206537+tydapchai@users.noreply.github.com>
2026-09-16 09:20:14 +0700

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minBuyin = prices[0];
+        int maxProfit = 0;
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i] < minBuyin){
+                minBuyin = prices[i];
+            }
+            maxProfit = Math.max(maxProfit, prices[i] - minBuyin);
+        }
+        return maxProfit;
+    }
+}
